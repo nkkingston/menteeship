72e024250768259b18deea86b6d1b3524a5ffec8
NKKingston <nkkingston@gmail.com>
2022-09-05 15:14:48 +0100

    calendar tool complete This commit allows users to input an office name and date and find out how many appoointments were booked.

--- lib/spreadsheet.rb
+++ lib/spreadsheet.rb
@@ -3,8 +3,17 @@ class Spreadsheet
     def initialize(file)
         @file = file
     end
-    def open_spreadsheet(file)
-        table = CSV.parse(File.read(file), headers: true)
+    def open_spreadsheet
+        print "Please enter the Office Group Name: "
+        user_input = gets.chomp
+        print "Please enter the date: "
+        user_date = gets.chomp
+        appointments = 0
+        offices = CSV.read('../calendar1.csv',headers:true)
+        offices.each do |office|
+            appointments += 1 if office["Office Group Name"] == user_input && office["Starts At"] == user_date
+        end
+        print "#{appointments} appointments booked at #{user_input} on #{user_date}"
     end
 end
 
